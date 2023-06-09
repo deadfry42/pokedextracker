d6d4fd92cff9cd553a5358b2e7a8a90ff3a22281
memyboi <forkknife0space@gmail.com>
2023-06-09 22:44:10 +0100

    select page started

---
+++ select/games.js
@@ -0,0 +1,22 @@
+const games = {
+    0: {
+        title: "Pokemon Box: Ruby and Sapphire",
+        description: "This is a Pokemon game released on the Nintendo Gamecube which stores pokemon. It is directly compatible with generation 3 pokemon games, making it great for making a living dex as it can transfer to every pokemon game (once the requirements are met for a certain game)",
+        subsiteurlname: "tracker",
+        imgLink: "https://archives.bulbagarden.net/media/upload/0/04/Box_RS_EN_boxart.png",
+        imgType: "gc",
+        compatible: [
+            1,
+        ]
+    },
+    1: {
+        title: "Pokemon Generation 3",
+        description: "This is a collection of Pokemon games all released on the Nintendo Gameboy Advance (SP) system. They consist of: Ruby, Sapphire, FireRed, LeafGreen and Emerald. ",
+        subsiteurlname: "tracker",
+        imgLink: "https://archives.bulbagarden.net/media/upload/0/04/Box_RS_EN_boxart.png",
+        imgType: "gba",
+        compatible: [
+            0, 2,
+        ]
+    }
+}
