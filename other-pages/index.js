36ab8fd4c63572d09e625951045ce4bfc32a4948
memyboi <forkknife0space@gmail.com>
2023-06-10 17:44:45 +0100

    github, netlfiy & google firebase added to o.pages

--- other-pages/index.js
+++ other-pages/index.js
@@ -1,5 +1,8 @@
 addEventListener("load", () => {
   document.getElementById("nerdlink").onclick = () => { window.location.href = "../info" }
+  document.getElementById("nerdlinkII").onclick = () => { window.location.href = "https://github.com/memyboi/pokedextracker" }
+  document.getElementById("nerdlinkIII").onclick = () => { window.location.href = "https://www.netlify.com" }
+  document.getElementById("nerdlinkIV").onclick = () => { window.location.href = "https://firebase.google.com" }
   document.getElementById("email").onclick = () => { window.location.href = "mailto:nk.personal.work@gmail.com" }
   document.getElementById("home").onclick = () => { window.location.href = "../" }
 })
